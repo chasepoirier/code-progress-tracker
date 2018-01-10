7188c4f694233698064cde15ddb43559f41bb266
Chase Poirier <chase.n.poirier@gmail.com>
2018-01-10 18:32:32 -0500

    added session timer and ability to change start date

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ let addInput = document.querySelector('.add-container .input');
 let subBtn = document.querySelector('.sub-container .btn');
 let subInput = document.querySelector('.sub-container .input');
 
+let changeDateBtn = document.querySelector('.settings .btn');
+let settingsBtn = document.querySelector('.settings-btn');
+let settingsCloseBtn = document.querySelector('.settings .close');
+let dateInput = document.querySelector('.settings .input');
+
+let sessionBtn = document.querySelector('.session-container .btn');
+let sessionInput = document.querySelector('.session-container .input');
+let startSession;
+
+sessionBtn.addEventListener('click', function(e) {	
+	if(sessionBtn.value === 'Start Session') {
+		let startTime = Date.now()
+
+		storeItem('current-session', startTime);
+		startSession = setInterval(() => countHours(startTime), 100);
+		sessionBtn.value = 'End Session';
+	} else {
+		sessionBtn.value = 'Start Session';
+		let updatedHours = convertToNumAndAdd(hours.innerHTML, sessionInput.value);
+		localStorage.removeItem('current-session');
+		storeItem('hours', updatedHours);
+		hours.innerHTML = updatedHours;
+		sessionInput.value = '';
+		clearInterval(startSession)
+	}
+});
+
 addBtn.addEventListener('click', function(e) {
 	e.preventDefault();
 	if(addInput.value) {
@@ -25,22 +52,55 @@ subBtn.addEventListener('click', function(e) {
 	}
 });
 
+changeDateBtn.addEventListener('click', function(e) {
+	e.preventDefault()
+	if(dateInput.value) {
+		let dateVal = dateInput.value.replace(/-/g, '/');
+		storeItem('start-date', dateVal);
+		changeDateBtn.parentNode.style.display = 'none';
+		settingsBtn.style.display = 'block';
+	}
+});
+
+settingsBtn.addEventListener('click', function(e) {
+	changeDateBtn.parentNode.style.display = 'flex';
+	settingsBtn.style.display = 'none';
+});
+
+settingsCloseBtn.addEventListener('click', function(e) {
+	changeDateBtn.parentNode.style.display = 'none';
+	settingsBtn.style.display = 'block';
+});
+
+
+
+// DOM CONTENT LOADED EVENT LISTENER
+
 document.addEventListener('DOMContentLoaded', function(e) {
+	changeDateBtn.parentNode.style.display = 'none';
+
 	if(localStorage.getItem('hours') === null) {
 		localStorage.setItem('hours', 0)
 	}
 
-	if(localStorage.getItem('count') === null) {
-		localStorage.setItem('count', 1)
+	if(localStorage.getItem('start-date') === null) {
+		changeDateBtn.parentNode.style.display = 'flex';
+		settingsBtn.style.display = 'none';
+	}
+
+	if(localStorage.getItem('current-session') !== null) {
+		let startTime = getStoredItem('current-session');
+
+		startSession = setInterval(() => countHours(startTime), 100);
+		sessionBtn.value = 'End Session';
 	}
 	
 	setInterval(() => countDays(), 100)	
 	
-	let updateCounter = getStoredItem('count');
 	let updatedHours = getStoredItem('hours');
 	hours.innerHTML = updatedHours;
-	counter.innerHTML = updateCounter;
 });
+
 
 function convertToNumAndAdd(current, value) {
 	let currentToNum = parseFloat(current);
@@ -56,6 +116,7 @@ function convertToNumAndSub(current, value) {
 	return Math.round((currentToNum - valueToNum)*100) / 100;
 }
 
+
 function storeItem(item, val) {
 	localStorage.setItem(item, val);
 }
@@ -66,14 +127,23 @@ function getStoredItem(item) {
 	return value;
 }
 
+function countHours(start) {
+	let currentTime = Date.now()
+	let hours = Math.abs(parseFloat(start) - currentTime) / 36e5;
+	sessionInput.value = hours.toFixed(4);
+}
+
 function countDays() {
+	
+	let startDay = new Date(getStoredItem('start-date'));
 
-	let oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-	let hours = 24*60*60;
-	let startDay = new Date(2018,00,10);
-	let currentTime = Date.now();
+	if(startDay) {
+		let oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+		
+		let currentTime = Date.now();
 
-	let diffDays = Math.abs((currentTime - startDay.getTime()) / oneDay) + 1;
+		let diffDays = Math.abs((currentTime - startDay.getTime()) / oneDay) + 1;
 
-	counter.innerHTML = diffDays.toFixed(6);
+		counter.innerHTML = diffDays.toFixed(6);
+	}
 }
